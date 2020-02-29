ba66d08d64199ba42dc3c1025d379de88163c62b
coderdojo <coderdojo@coderdojo>
2020-02-29 11:31:43 +0000

    Shooting shooting stars -- Meteor showers

--- Oisin/escape-blizzard.py
+++ Oisin/escape-blizzard.py
@@ -13,7 +13,7 @@ MAXSPEED = 1
 MOVERATE = 2
 flakespeed = 1
 NBLINES = 1
-NBSNOW = 50
+NBSNOW = 90
 PLAYERSIZE=3
 
 MAXWIND = 0
@@ -48,17 +48,16 @@ def runGame():
                  'y': WINHEIGHT-PLAYERSIZE,
                  'size': PLAYERSIZE,
                  'color' : (255,0,0),
+                 'maxbullets': 2
                  }
 
     flakes = []
     for n in range(NBSNOW):
-      color = random.randint(192, 255)
-      flakes.append( {
-                 'x': random.randint(0,WINWIDTH-1),
-                 'y': random.randint(-WINHEIGHT,0),
-                 'color' : (color,color,color),
-                 } )
-                  
+      flakes.append( newflake() )
+
+ 
+    bullets = []
+                 
     moveLeft  = False
     moveRight = False
     moveUp    = False
@@ -74,10 +73,10 @@ def runGame():
         if moveDown : player['y'] = player['y']+2
         if moveLeft : player['x'] = player['x']-2
         if moveRight : player['x'] = player['x']+2
-          
-        for n in range(NBSNOW):
-            flake = flakes[n]
-            
+        player['rect'] = pygame.Rect( (player['x'], player['y'], 2, 2) )
+
+        for flake in flakes:
+           
 	        # Get flake new position
             new_x = flake['x'] + wind
             new_y = flake['y'] + random.randint(0,MAXSPEED)+(flakespeed)
@@ -97,7 +96,6 @@ def runGame():
                 
 	        # Draw flake
             DISPLAYSURF.blit(SNOW_IMG, (flake['x'],flake['y'], flake['x']+50,flake['y']+50))
-            player['rect'] = pygame.Rect( (player['x'], player['y'], 2, 2) )
             flake['rect'] = pygame.Rect( (flake['x'], flake['y'], 40, 40) )
             
             if player['rect'].colliderect(flake['rect']):
@@ -108,9 +106,37 @@ def runGame():
 	        # If the color is not black, we move the flake
             if new_y > WINHEIGHT:
 
-	       # Otherwise, we move the flake back to the top
+	           # Otherwise, we move the flake back to the top
                flake['x'] = random.randint(0,WINWIDTH-1)
                flake['y'] = random.randint(-WINHEIGHT//2,0)
+        for bullet in bullets:
+			# Get bullet new position
+            new_y = bullet['y'] - (bullet['speed'])
+
+	        # Make sure the bullet stays on screen
+            if new_y < 0:
+				bullets.remove(bullet)
+            else:	
+                bullet['rect'] = pygame.Rect( (bullet['x'], bullet['y'], bullet['size'], bullet['size']) )
+				
+                for flake in flakes:
+               
+                  flake['rect'] = pygame.Rect( (flake['x'], flake['y'], 40, 40) )
+            
+                  if bullet['rect'].colliderect(flake['rect']):
+                    flake['x'] = random.randint(0,WINWIDTH-1)
+                    flake['y'] = random.randint(-WINHEIGHT//2,0)
+                    bullets.remove (bullet)
+                    for n in range (10):
+                     flakes.append( newflake() )
+                     flakes.append( newflake() )
+                    
+                    
+
+            bullet['y']= new_y
+                 
+            pygame.draw.rect( DISPLAYSURF, bullet['color'], (bullet['x'], bullet['y'], bullet['size'], bullet['size'] ))
+
 
         pygame.draw.rect( DISPLAYSURF, red, (player['x'], player['y'], player['size'], player['size'] ))
            
@@ -131,9 +157,17 @@ def runGame():
                 elif event.key in (K_RIGHT, K_d):
                     moveLeft = False  
                     moveRight = True
-		elif event.key == K_SPACE:
-                    pygame.draw.rect(DISPLAYSURF, black, (0,0,WINWIDTH,WINHEIGHT)) 
-		    bottom_line = [ WINHEIGHT ] * WINWIDTH 
+                elif event.key == K_SPACE:
+					
+                 if len(bullets)<player['maxbullets']:
+                  bullets.append( {
+                   'x': player['x'],
+                   'y': player['y'],
+                   'color' : white,
+                   'size' : 3,
+                   'speed' : 1,
+                 } )
+
             elif event.type == KEYUP:
  
                 if event.key in (K_LEFT, K_a):
@@ -155,6 +189,17 @@ def runGame():
                      
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+        
+        
+def newflake():
      
+    color = random.randint(192, 255)
+    flake = {
+                 'x': random.randint(0,WINWIDTH-1),
+                 'y': random.randint(-WINHEIGHT,0),
+                 'color' : (color,color,color),
+                 }
+    return flake
+                 
 if __name__ == '__main__':
     main()
